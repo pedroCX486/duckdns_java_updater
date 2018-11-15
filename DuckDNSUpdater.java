293d24ef4839e3b38d6b500494bae27425df333b
Pedro Carneiro <44985060+pedroCX486@users.noreply.github.com>
2018-11-15 19:53:30 -0200

    Code cleanup

--- DuckDNSUpdater.java
+++ DuckDNSUpdater.java
@@ -1,63 +1,63 @@
-import java.net.*;
-import java.io.*;
+import java.io.PrintStream;
 
-public class DuckDNSUpdater {
-
-    public static void main(String[] args) {
-
-        String token = "YOUR_DUCK_DNS_TOKEN";
-        String domain = "YOURDOMAIN";
-
-        System.out.println("DuckDNS IP Updater Script\r\n");
-		
+public class DuckDNSUpdater{
+ 
+  public static void main(String[] args) {
+        String token = "YOUR_DUCK_DNS_TOKEN"; //Your DuckDNS token, present on your profile
+        String domain = "YOURDOMAIN"; //Your domain name, eg "myhome" instead of "myhome.duckdns.org"
+        
+        System.out.println("DuckDNS IP Updater Script");
+        
         updateDuckDNS(domain, token);
-		
         System.exit(0);
     }
-
-    static void updateDuckDNS(String domain, String token){
+  
+  static void updateDuckDNS(String domain, String token) {
         try {
-            URL url = new URL("https://www.duckdns.org/update?domains="+ domain +"&token="+ token +"&ip="+ getIP());
-
+            java.net.URL url = new java.net.URL("https://www.duckdns.org/update?domains=" + domain + "&token=" + token + "&ip=" + getIP());
+            
             System.out.println("Sending request to DuckDNS...");
-
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            
+            java.net.HttpURLConnection con = (java.net.HttpURLConnection)url.openConnection();
             con.setRequestMethod("GET");
-
-            if(con.getResponseCode() == 200){
+        
+            if (con.getResponseCode() == 200) {
                 System.out.println("Success! Your IP was updated on DuckDNS!");
-            }else{
-                System.out.println("DuckDNS returned HTTP Error " + con.getResponseCode() + "! Try running the application again or checking your internet connection.");
+            } else {
+                System.out.println("Error. Try running the application again or checking your internet connection.");
             }
-
-            con.disconnect();
-
-        }catch (Exception e) {
+        
+        con.disconnect();
+        }
+        catch (Exception e) {
             System.out.println(e);
             System.out.println("Error. Try running the application again or checking your internet connection.");
             System.exit(1);
         }
     }
+  
+  static String getIP() {
 
-    static String getIP(){
-		
         String publicIP;
-		
-        System.out.println("Fetching your public IP address...");
-		
-        try{
-            URL myPublicIP = new URL("http://checkip.amazonaws.com");
-            BufferedReader in = new BufferedReader(new InputStreamReader(myPublicIP.openStream()));
+        
+        System.out.println("\r\nFetching your public IP address...");
+
+        try {
+            java.net.URL myPublicIP = new java.net.URL("http://checkip.amazonaws.com");
+            java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(myPublicIP.openStream()));
             publicIP = in.readLine();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
-            publicIP = "Couldn't fetch IP address. Try running the application again or checking your internet connection.";
-            System.out.println("Error: " + publicIP);
-            System.exit(1);
+            publicIP = "Error. Couldn't fetch your public IP address. Try running the application again or checking your internet connection.";
         }
 
-        System.out.println("Your public IP is: " + publicIP);
-      
+        if (publicIP.contains("Error")) {
+            System.out.println(publicIP);
+            System.exit(1);
+        }else{
+            System.out.println("Your public IP is: " + publicIP);
+        }
+        
         return publicIP;
     }
 }
